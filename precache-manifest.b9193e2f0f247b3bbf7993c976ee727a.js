6ed76262e63792dc765e0845bfecbda266a631c2
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-08-09 01:49:25 -0500

    Updates

--- precache-manifest.b9193e2f0f247b3bbf7993c976ee727a.js
+++ precache-manifest.b9193e2f0f247b3bbf7993c976ee727a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ddc483d7be1f0fa935612e58365858c8",
+    "revision": "dcbb9d3c2ee1902f242372f1823fb786",
     "url": "/katelyn-lopes/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/katelyn-lopes/static/css/2.11829350.chunk.css"
   },
   {
-    "revision": "c34c8b41af6fec46e8bf",
+    "revision": "4facc7dd5f817d2dca76",
     "url": "/katelyn-lopes/static/css/main.56619765.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/katelyn-lopes/static/js/2.90a94064.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c34c8b41af6fec46e8bf",
-    "url": "/katelyn-lopes/static/js/main.623810e2.chunk.js"
+    "revision": "4facc7dd5f817d2dca76",
+    "url": "/katelyn-lopes/static/js/main.ca1b2cc4.chunk.js"
   },
   {
     "revision": "6c2535e6babac1a578de",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/katelyn-lopes/precache-manifest.b5e7baf484dad36f0c91bcbf309b8ba5.js"
+  "/katelyn-lopes/precache-manifest.b9193e2f0f247b3bbf7993c976ee727a.js"
 );
 
 self.addEventListener('message', (event) => {
