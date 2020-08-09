ab87d8cb8c92f2b47be73fa2a533e271468eb089
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-08-09 01:40:51 -0500

    update tab colors

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
 
 function Navigation() {
 
     return (
-            <div className="row navigation" id="navigation">
-                <Link to="/home">
-                <h1 class="logo">K<span>L</span></h1>
-                </Link>
-                <Link to="/about" className="" >About</Link>
-                <Link to="/portfolio" className="">Portfolio</Link>
-                <Link to="/contact" className="">Contact</Link>
-                <Link to="/resume" className="">Resume</Link>
-            </div>
+        <Nav variant="pills" defaultActiveKey="/" className="row navigation" id="navigation">
             
+            <Nav.Item>
+                <Nav.Link eventKey="/home" href="/#home">
+                    <h1 class="logo">K<span>L</span></h1>
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href="/#about" className="" >About</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="/portfolio" href="/#portfolio">Portfolio</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="/contact" href="/#contact">Contact</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="/resume" href="/#resume">Resume</Nav.Link>
+            </Nav.Item>
+        </Nav>     
 
     );
 };
