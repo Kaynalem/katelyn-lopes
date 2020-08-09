12936d13b071f26a2d090c01c83cf33b38983aae
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-08-08 23:51:04 -0500

    Updates

---
+++ precache-manifest.ed230418686999557d53c426407ece69.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77a270763076abfbc8a4686d60dd7533",
+    "url": "/katelyn-lopes/index.html"
+  },
+  {
+    "revision": "2571e36e1dc0bcdb3c48",
+    "url": "/katelyn-lopes/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c340f31911faf6164f2e",
+    "url": "/katelyn-lopes/static/css/main.26f7777f.chunk.css"
+  },
+  {
+    "revision": "2571e36e1dc0bcdb3c48",
+    "url": "/katelyn-lopes/static/js/2.e3b94f10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/katelyn-lopes/static/js/2.e3b94f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c340f31911faf6164f2e",
+    "url": "/katelyn-lopes/static/js/main.c7435032.chunk.js"
+  },
+  {
+    "revision": "6c2535e6babac1a578de",
+    "url": "/katelyn-lopes/static/js/runtime-main.307d17f0.js"
+  },
+  {
+    "revision": "2885b87b51c5f721f5056585926e561d",
+    "url": "/katelyn-lopes/static/media/cover-image.2885b87b.jpg"
+  }
+]);
