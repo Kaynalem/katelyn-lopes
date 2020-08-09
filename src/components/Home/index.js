9d8e12ef89e3a4435dfa8d6d4c5e72284e2b3df9
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-08-09 00:10:45 -0500

    fix home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,13 +3,15 @@ import React, { Component } from 'react';
 class Home extends Component {
     render() {
         return(
-
-        <div className="row hero">
-                    <div className="hero-text">
-                        <h1>KATELYN <span>LOPES</span></h1>
-                        <h3>CODING BOOTCAMP STUDENT</h3>
-                    </div>
+            <section>
+            <div className="row hero">
+                <div className="hero-text">
+                    <h1>KATELYN <span>LOPES</span></h1>
+                    <h3>CODING BOOTCAMP STUDENT</h3>
                 </div>
+            </div>
+            </section>
+        
 
         );
     }
